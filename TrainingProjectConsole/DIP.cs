6c9110bbeff7cc0f477d4b81e5489f8a9f254afa
sulton <sultonmalik326@gmail.com>
2024-05-26 12:20:23 +0500

    Add project files.

---
+++ TrainingProjectConsole/DIP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+   
+
+   
+
+    public interface IEMPDataAccessLayer
+    {
+        public void SaveEmployee();
+    }
+
+    public class EMPDataAccessLayer : IEMPDataAccessLayer
+    {
+        public void SaveEmployee()
+        {
+            //Code for save employee
+        }
+    }
+
+    public class Employee
+    {
+        private readonly IEMPDataAccessLayer _EMPDataAccessLayer;
+        public Employee(IEMPDataAccessLayer EMPDataAccessLayer)
+        {
+            _EMPDataAccessLayer = EMPDataAccessLayer;
+        }
+        public void SaveEmployee()
+        {
+            _EMPDataAccessLayer.SaveEmployee();
+        }
+    }
+}
